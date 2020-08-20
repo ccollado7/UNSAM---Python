6e193726ad440abaa364dbc83bd96de848678e5a
Collado Claudio <46108725+ccollado7@users.noreply.github.com>
2020-08-20 20:55:07 -0300

    Revision de Pares - Semana N°1

---
+++ Revision de Pares/Semana N°1/01-3-nco-geringoso.py-fra-2020-08-09_03.48.21.py
@@ -0,0 +1,19 @@
+cadena = "frio"
+capadepenapa = ''
+for caracter in cadena:
+    if caracter=='a':
+        capadepenapa=capadepenapa+'apa'
+    elif caracter =='e':
+        capadepenapa=capadepenapa+'epe'
+    elif caracter == 'i':
+            capadepenapa=capadepenapa+'ipi'
+    elif caracter== 'o':
+            capadepenapa=capadepenapa+'opo'
+    elif caracter=='u':
+            capadepenapa=capadepenapa+'upu'
+    else:
+            capadepenapa=capadepenapa+caracter
+    
+print(capadepenapa)
+
+#geperipingoposopo
