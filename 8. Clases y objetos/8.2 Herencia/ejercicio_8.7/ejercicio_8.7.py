4c986e3f363c8e26af5d0446bdf3237619fe86a1
Collado Claudio <46108725+ccollado7@users.noreply.github.com>
2020-10-08 09:44:06 -0300

    Semana N°8

---
+++ 8. Clases y objetos/8.2 Herencia/ejercicio_8.7/ejercicio_8.7.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 20:40:23 2020
+
+@author: User
+"""
+
+import informe
+
+informe.informe_camion('camion.csv', 'precios.csv',fmt = 'txt')
+
+#%%
+
+informe.informe_camion('camion.csv', 'precios.csv',fmt = 'csv')
+
+#%%
+
+informe.informe_camion('camion.csv', 'precios.csv',fmt = 'html')
